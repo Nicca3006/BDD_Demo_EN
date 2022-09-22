5ce1920dad68455b442b209b1e95f6149ed696eb
nicca3006 <github@rabow.eu>
2022-09-22 18:23:47 +0200

    Start des Vortrags JUG Saxony Day

--- src/test/java/eu/rabow/bdd/DatenfelderSteps.java
+++ src/test/java/eu/rabow/bdd/DatenfelderSteps.java
@@ -13,45 +13,12 @@ import static org.junit.Assert.*;
 public class DatenfelderSteps {
     Person person;
 
-    @Angenommen("eine Person wird angelegt")
-    public void einePersonWirdAngelegt() {
-        throw new PendingException();
-    }
-
-    @Angenommen("eine Person gibt ihr Geburtsdatum ein")
-    public void einePersonGibtIhrGeburtsdatumEin() {
-        throw new PendingException();
-    }
-
-    @Angenommen("die Person gibt ihren Namen ein")
-    public void die_person_gibt_ihren_namen_ein() {
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Wenn("der Name nicht eingetragen wird")
-    public void derNameNichtEingetragenWird() {
-        person.setName("");
-    }
-
-    @Dann("wird die Meldung {string} ausgegeben")
-    public void wirdDieMeldungAusgegeben(String fehlermeldung) {
-        assertTrue (validiere(person).contains(fehlermeldung));
-    }
-
-    @Wenn("das Geburtsdatum nicht eingetragen wird")
-    public void dasGeburtsdatumNichtEingetragenWird() {
-        person.setGeburtsdatum("");
-    }
     @Wenn("der Name nicht mehr als {int} Zeichen hat")
     public void der_name_nicht_mehr_als_zeichen_hat(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Dann("wird der Name gespeichert")
-    public void wird_der_name_gespeichert() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
     @Wenn("der Tag der Geburt unbekannt ist")
     public void der_tag_der_geburt_unbekannt_ist() {
         // Write code here that turns the phrase above into concrete actions
@@ -60,12 +27,6 @@ public class DatenfelderSteps {
 
     @Dann("kann der Tag leer bleiben")
     public void kann_der_tag_leer_bleiben() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Dann("wird das Geburtsdatum gespeichert")
-    public void wird_das_geburtsdatum_gespeichert() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
